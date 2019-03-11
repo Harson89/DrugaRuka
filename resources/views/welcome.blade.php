255cb2581fcedaac36825589e4967ce43655c89b
Harson89 <37156656+Harson89@users.noreply.github.com>
2019-03-11 13:00:32 +0100

    Proba 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Druga Ruka Shop
                 </div>
 
                 <div class="links">
